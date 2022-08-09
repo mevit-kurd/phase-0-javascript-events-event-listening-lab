d987fa554ad93fc7a29616aa33e83f4cbef89248
MevanAhmed <mevanovic@gmail.com>
2022-08-09 13:33:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+  const input = document.querySelector('input');
+    function clickAlert() {
+    alert('I was clicked!');
+   }
+
+input.addEventListener('click', clickAlert);
+
 }
+
+addingEventListener()
